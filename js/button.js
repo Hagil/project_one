717705552b8bb4bc41a81122c86e8c681b438b52
Hagil Baik <hagilbaik@gmail.com>
2017-10-09 16:37:11 +0800

    There will always be beer cans rolling on the floor of your car when the boss asks for a lift home from the office.

--- js/button.js
+++ js/button.js
@@ -5,20 +5,24 @@ console.log('Inside set_button');
 $('#start').click(start);
 $('#reset').click(reset);
 }
+
 function start(){
   console.log('start_game');
 
 }
-function reset_game(){
-  new Phaser.Game(800, 600, Phaser.CANVAS, 'location', {
-    preload: preload,
-    create: create,
-    update: update,
-    render: render
-  });
-}
+
 function reset(){
   console.log('reset_game');
-  clear();
-reset_game();
+  document.getElementById('location').addEventListener('click', function() {
+    context.clearRect(0,0, location[0].width, location[0].height);
+  });
+  //reset_game();
 }
+//function reset_game(){
+//  new Phaser.Game(800, 600, Phaser.CANVAS, 'location', {
+//    preload: preload,
+//    create: create,
+//    update: update,
+//    render: render
+//  });
+//}
